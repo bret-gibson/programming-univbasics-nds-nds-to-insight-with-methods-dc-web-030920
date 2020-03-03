5e3c78a2054ba1d378b1da6d60711c5acebf22e4
Bret Gibson <bret.gibson@gmail.com>
2020-03-03 21:24:45 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -5,11 +5,11 @@ require 'directors_database'
 # using director_data as input
 def gross_for_director(director_data)
   row_index = 0
-    while row_index < nds.length do
+    while row_index < director_data.length do
       column_index = 0
       grand_total = 0
-      while column_index < nds[row_index][:movies].length do
-        grand_total += nds[row_index][:movies][column_index][:worldwide_gross]
+      while column_index < director_data[row_index][:movies].length do
+        grand_total += director_data[row_index][:movies][column_index][:worldwide_gross]
         column_index += 1
       end
       row_index += 1
