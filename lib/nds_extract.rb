1c2160aa2add9f4808224cd6f478a80e50fb26bd
Bret Gibson <bret.gibson@gmail.com>
2020-03-03 22:03:52 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -9,6 +9,7 @@ def gross_for_director(director_data)
   column_index = 0
   grand_total = 0;
   while column_index < director_data[:name][:movies].length do
+    puts director_data[:name][:movies].length
     grand_total += director_data[:name][:movies][column_index][:worldwide_gross]
     column_index += 1
   end
